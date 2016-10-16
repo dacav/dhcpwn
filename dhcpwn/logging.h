3867e73b3e13b65dc3edc99b9d6bb5bae0d7d51b
Giovanni Simoni <dacav@openmailbox.org>
2016-10-16 15:40:00 +0200

    Added logging pointer

--- dhcpwn/logging.h
+++ dhcpwn/logging.h
@@ -11,6 +11,8 @@ typedef enum {
 
 void log_setup(FILE *stream, log_level_t level);
 
+typedef void (*log_cb_t)(const char *fmt, ...);
+
 void log_debug(const char *fmt, ...);
 void log_info(const char *fmt, ...);
 void log_error(const char *fmt, ...);
