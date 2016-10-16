9cbca4fc049438985c2a891f4b0dc0b87a56a325
Giovanni Simoni <dacav@openmailbox.org>
2016-10-16 15:40:25 +0200

    Scaffold of threaded sniffer

--- dhcpwn/rogue.c
+++ dhcpwn/rogue.c
@@ -2,9 +2,13 @@
 #include "util.h"
 #include "simplecap.h"
 #include "const.h"
+#include "logging.h"
+#include "repr.h"
 
 #include <stdlib.h>
 #include <string.h>
+#include <pthread.h>
+#include <errno.h>
 
 struct rogue {
     struct {
@@ -13,12 +17,18 @@ struct rogue {
     } conf;
 
     struct {
-        pcap_t *pcap;
+        pcap_t *cap;
+        pthread_t thread;
     } sniffer;
+
+    char *tostr;
 };
 
 static void sniffer_teardown(rogue_t);
 static int sniffer_setup(rogue_t);
+static void * sniffer_thread(void *args);
+static void sniffer_loop(u_char *arg, const struct pcap_pkthdr *hdr,
+                         const u_char *bytes);
 
 rogue_t rogue_new_ethrnd()
 {
@@ -30,6 +40,7 @@ rogue_t rogue_new(const uint8_t *macaddr, size_t maclen)
     rogue_t out = malloc(sizeof(struct rogue));
     if (out == NULL) goto fail;
 
+    memset((void *)out, 0, sizeof(struct rogue));
     out->conf.macaddr = calloc(maclen, sizeof(uint8_t));
     if (out->conf.macaddr == NULL) goto fail;
     if (macaddr != NULL) {
@@ -50,8 +61,21 @@ void rogue_del(rogue_t r)
 {
     if (r == NULL) return;
     sniffer_teardown(r);
+    free(r->tostr);
     free(r->conf.macaddr);
     free(r);
+}
+
+const char *rogue_tostr(rogue_t r)
+{
+    if (r->tostr == NULL) {
+        const char *prefix = "rogue(";
+        const char *suffix = ")";
+        r->tostr = repr_bytes(r->conf.macaddr, r->conf.maclen,
+            ":", prefix, suffix
+        );
+    }
+    return r->tostr;
 }
 
 static int sniffer_setup(rogue_t r)
@@ -61,19 +85,52 @@ static int sniffer_setup(rogue_t r)
     char *f2 = NULL;
     char *f3 = NULL;
 
-    r->sniffer.pcap = spcap_new(DHCPWN_Interface);
-    if (r->sniffer.pcap == NULL) goto exit;
+    r->sniffer.cap = spcap_new(DHCPWN_Interface);
+    if (r->sniffer.cap == NULL) {
+        goto exit;
+    }
     
     f1 = spcap_mkfilter_ether_dst(r->conf.macaddr, r->conf.maclen);
-    if (f1 == NULL) goto exit;
+    if (f1 == NULL) {
+        goto exit;
+    }
 
     f2 = spcap_mkfilter_proto_dst(SPCAP_PROTO_UDP, DHCPWN_InboundPort);
-    if (f2 == NULL) goto exit;
+    if (f2 == NULL) {
+        goto exit;
+    }
 
     f3 = spcap_mkfilter_and(f1, f2);
-    if (f3 == NULL) goto exit;
+    if (f3 == NULL) {
+        goto exit;
+    }
 
-    result = spcap_setfilter(r->sniffer.pcap, f3);
+    if (spcap_setfilter(r->sniffer.cap, f3) == -1) {
+        goto exit;
+    }
+
+    pthread_attr_t attr;
+    if (pthread_attr_init(&attr) != 0) {
+        goto exit;
+    }
+
+    result = pthread_create(
+        &r->sniffer.thread,
+        &attr,
+        sniffer_thread,
+        (void *)r
+    );
+
+    if (result < 0) {
+        log_error("Creating thread for %s: %s",
+            rogue_tostr(r),
+            strerror(result)
+        );
+    }
+
+    if (pthread_attr_destroy(&attr) != 0) {
+        result = -1;
+    }
 
   exit:
     free(f1);
@@ -84,5 +141,73 @@ static int sniffer_setup(rogue_t r)
 
 static void sniffer_teardown(rogue_t r)
 {
-    pcap_close(r->sniffer.pcap);
+    int ret;
+    const char *tostr = rogue_tostr(r);
+
+    log_debug("%s: closing pcap", tostr);
+    pcap_breakloop(r->sniffer.cap);
+
+    ret = pthread_cancel(r->sniffer.thread);
+    if (ret != 0) {
+        log_error("%s: thread cancel error, %s", tostr, strerror(ret));
+    }
+
+    log_debug("%s: joining thread", tostr);
+    ret = pthread_join(r->sniffer.thread, NULL);
+    if (ret != 0) {
+        log_error("%s; termination error, %s", tostr, strerror(ret));
+    }
+
+    pcap_close(r->sniffer.cap);
+}
+
+static void sniffer_loop(u_char *arg, const struct pcap_pkthdr *hdr,
+                         const u_char *bytes)
+{
+    rogue_t r = (rogue_t)arg;
+
+    log_debug("%s: received bytes...", rogue_tostr(r));
+}
+
+static void * sniffer_thread(void *arg)
+{
+    rogue_t r = (rogue_t)arg;
+    const int forever = -1;
+    const char *tostr = rogue_tostr(r);
+
+    log_debug("%s: thread starting loop", tostr);
+    int result = pcap_loop(
+        r->sniffer.cap,
+        forever,
+        sniffer_loop,
+        (u_char *)arg
+    );
+
+    log_cb_t logger;
+    const char *reason;
+
+    switch (result) {
+        case 0:
+            logger = log_info;
+            reason = "no more packets";
+            break;
+        case -1:
+            logger = log_error;
+            reason = pcap_geterr(r->sniffer.cap);
+            break;
+        case -2:
+            logger = log_info;
+            reason = "interrupted via pcap_break_loop";
+            break;
+        default:
+            logger = log_error;
+            reason = "unknown reason";
+    }
+    logger("%s: interrupted, %s (%d)",
+        tostr,
+        reason,
+        result
+    );
+
+    return NULL;
 }
--- dhcpwn/rogue.h
+++ dhcpwn/rogue.h
@@ -9,5 +9,6 @@ typedef struct rogue * rogue_t;
 rogue_t rogue_new_ethrnd();
 rogue_t rogue_new(const uint8_t *macaddr, size_t maclen);
 void rogue_del(rogue_t);
+const char * rogue_tostr(rogue_t);
 
 #endif // _DHCPWN_ROGUE_H
